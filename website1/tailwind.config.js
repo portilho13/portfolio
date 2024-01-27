ad5c827cf3187d4de3063bfaee325641850b12d3
portilho13 <junior.portilho2005@gmail.com>
2024-01-27 11:10:45 +0000

    Finished project

---
+++ website1/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    fontFamily: {
+      'baloo-bhai': ['Baloo Bhai', 'sans-serif'],
+      'roboto': ['Roboto', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
